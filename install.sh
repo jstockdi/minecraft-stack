1cb61c183f215b5b6ffa3183585440fb0ffb26ae
Jonathan Stockdill <jon.stockdill@gmail.com>
2024-05-27 22:49:59 -0400

    Zeldus

--- install.sh
+++ install.sh
@@ -17,10 +17,19 @@ rm -rf /opt/jdk-21.0.3
 mv jdk-21.0.3 /opt
 
 export JAVA_HOME=/opt/jdk-21.0.3
-echo 'export JAVA_HOME='$JAVA_HOME >> ~/.bashrc
+echo 'export JAVA_HOME='$JAVA_HOME >> /home/ec2-user/.bashrc
 
-## server.jar
+
+
+## SERVER JAR
 
 mkdir -p /opt/minecraft/jars
 
 curl -s https://piston-data.mojang.com/v1/objects/145ff0858209bcfc164859ba735d4199aafa1eea/server.jar > /opt/minecraft/jars/server_1.20.6.jar
+
+
+## ZELDUS
+
+mkdir -p /opt/minecraft/servers/zeldus
+echo 'eula=true' > /opt/minecraft/servers/zeldus/eula.txt
+cd /opt/minecraft/servers/zeldus && ln -s /opt/minecraft/jars/server_1.20.6.jar server.jar 
