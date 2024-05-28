1f78034a9d47ca6b1ec3f4401a8a5474dd5426d2
Jonathan Stockdill <jon.stockdill@gmail.com>
2024-05-27 22:31:54 -0400

    Adding jdk

--- install.sh
+++ install.sh
@@ -9,5 +9,8 @@ mkdir -p $INSTALL_DIR
 cd $INSTALL_DIR 
 
 curl "https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz" > jdk-21_linux-x64_bin.tar.gz 
+tar -xzf jdk-21_linux-x64_bin.tar.gz
 
+rm -rf /opt/jdk-21.0.3
+mv jdk-21.0.3 /opt
 
